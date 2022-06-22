4e650c8e464a8a67a6ecc113e5887b5549bcb700
Muhammad Mamoon Uddin <mamoonuddin17@gmail.com>
2022-06-22 15:51:27 -0500

    Just A Basic Skeleton.

---
+++ FrontEnd/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="images/homePage.css">
+</head>
+<body>
+
+<img src="img_la.jpg" alt="LA" style="width:100%">
+
+<div class="">
+
+<p>This is going to be a checking form for login</p>
+<form action="register.php">
+    <input type="submit" value="Go to Register" />
+</form>
+</div>
+
+</body>
+</html>
+
