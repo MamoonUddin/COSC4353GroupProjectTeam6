aed75050d9c8fbe35c134fd892cf484295efe44b
psudario <phillipsudario21@gmail.com>
2022-07-01 06:06:47 -0500

    file restructure

---
+++ client/FrontEnd/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="">
+<style>
+</style>
+<script src=""></script>
+<body>
+
+<img src="img_la.jpg" alt="LA" style="width:100%">
+
+<div class="">
+
+ <p>This is going to be a checking form for making a account</p>
+
+</div>
+
+</body>
+</html>
+
