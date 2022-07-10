e42e3b2eb10d1d192bbf0e04bb435221ba327396
Victor Artiles <victormanuellepro@gmail.com>
2022-07-10 18:47:39 -0500

    Made new page for pricing and just added a "pricing class" place holder for the assignment

---
+++ server/routes/pricing.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const pricingRouter = express.Router();
+
+const pricing = require("../tst/pricing.json").pricing;
+
+pricingRouter.get("/", (req, res) => {
+    
+   //PLACEHOLDER TODO
+    
+});
